1a20562c1e26db01e7a6c8859ed6bd75b209b1c0
antonellla <antonella.c.wilby@gmail.com>
2026-01-24 19:47:00 -0800

    add sliding pane for detail overlay

--- neon-map/src/App.tsx
+++ neon-map/src/App.tsx
@@ -1,57 +1,56 @@
-import * as React from 'react';
-import {useState, useMemo} from 'react';
-import {createRoot} from 'react-dom/client';
+import React, { useState, useMemo } from 'react';
+import { createRoot } from 'react-dom/client';
+import SlidingPane from "react-sliding-pane";
+import "react-sliding-pane/dist/react-sliding-pane.css";
 import {
   Map,
   Marker,
   Popup,
   NavigationControl,
   FullscreenControl,
-  ScaleControl,
-  GeolocateControl
+  ScaleControl
 } from 'react-map-gl/maplibre';
 
-import Pin from './pin';
+import Pin from './components/pin/pin';
 
 import LOCATIONS from '../../.data/locations.json';
 
 export default function App() {
   const [popupInfo, setPopupInfo] = useState(null);
 
-  const pins = useMemo(
-    () =>
-      LOCATIONS.map((location, index) => (
-        <Marker
-          key={`marker-${index}`}
-          longitude={location.longitude}
-          latitude={location.latitude}
-          anchor="bottom"
-          onClick={e => {
-            // If we let the click event propagates to the map, it will immediately close the popup
-            // with `closeOnClick: true`
-            e.originalEvent.stopPropagation();
-            setPopupInfo(location);
-          }}
-        >
-          <Pin />
-        </Marker>
-      )),
-    []
+  const [state, setState] = useState({
+    isPaneOpen: false,
+    locationName: null,
+    location: null,
+  });
+
+  const pins = useMemo( () =>
+    LOCATIONS.map((location, index) => (
+      <Marker key={`marker-${index}`}
+              longitude={location.longitude}
+              latitude={location.latitude}
+              anchor="bottom"
+              onClick={event => {
+                event.originalEvent.stopPropagation();
+                setPopupInfo(location);
+              }}>
+        <Pin />
+      </Marker>
+    )),
+  []
   );
 
   return (
     <>
-      <Map
-        initialViewState={{
-          latitude: 34.1,
-          longitude: -118.1,
-          zoom: 11,
-          bearing: 0,
-          pitch: 0
-        }}
-        mapStyle="https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"
-      >
-        <GeolocateControl position="top-left" />
+      <Map initialViewState={{
+             latitude: 34.1,
+             longitude: -118.25,
+             zoom: 11,
+             bearing: 0,
+             pitch: 0
+           }}
+           mapStyle="https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json">
+
         <FullscreenControl position="top-left" />
         <NavigationControl position="top-left" />
         <ScaleControl />
@@ -59,19 +58,43 @@ export default function App() {
         {pins}
 
         {popupInfo && (
-          <Popup
-            anchor="top"
-            longitude={Number(popupInfo.longitude)}
-            latitude={Number(popupInfo.latitude)}
-            onClose={() => setPopupInfo(null)}
-          >
+          <Popup maxWidth="340px"
+                 anchor="top"
+                 longitude={Number(popupInfo.longitude)}
+                 latitude={Number(popupInfo.latitude)}
+                 onClose={() => setPopupInfo(null)}>
             <div>
-              {popupInfo.location} |{' '}
+              <span class="popup-title">{popupInfo.name}</span><br />
+              <span class="popup-location">{popupInfo.location}</span>
             </div>
-            <img width="100%" src={popupInfo.image} />
+            <img width="100%" src={popupInfo.thumb} />
+
+            <a href="#" onClick={() => 
+              setState({ 
+                isPaneOpen: true,
+                locationName: popupInfo.name,
+                location: popupInfo.location
+              })}>
+              see more
+            </a>
           </Popup>
         )}
+
       </Map>
+      
+      <SlidingPane className="detail-pane"
+                   overlayClassName="detail-overlay"
+                   isOpen={state.isPaneOpen}
+                   title={state.locationName}
+                   subtitle={state.location}
+                   closeIcon="x"
+                   onRequestClose={() => {
+                    setState({ isPaneOpen: false });
+                   }} 
+                   children={undefined}>
+        <div></div>
+
+      </SlidingPane>
 
     </>
   );
